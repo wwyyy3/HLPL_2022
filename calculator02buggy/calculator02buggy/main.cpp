608d521f8bd859245617ad227a66809c79d8b2eb
wwyyy3 <77263425+wwyyy3@users.noreply.github.com>
2022-03-07 08:43:13 +0100

    drill.2

--- calculator02buggy/calculator02buggy/main.cpp
+++ calculator02buggy/calculator02buggy/main.cpp
@@ -17,18 +17,18 @@
 
 */
 
-#include "../std_lib_facilities.h"
+#include "std_lib_facilities.h"
 
 //------------------------------------------------------------------------------
 
-lass Token{
+class Token{
 public:
     char kind;        // what kind of token
     double value;     // for numbers: a value
     Token(char ch)    // make a Token from a char
-        :kind(ch), value(0) { }
+        :kind(ch), value(0) { } //---line break
     Token(char ch, double val)     // make a Token from a char and a double
-        :kind(ch), value(val) { }
+        :kind(ch), value(val) { }//---line break
 };
 
 //------------------------------------------------------------------------------
@@ -47,9 +47,8 @@ private:
 
 // The constructor just sets full to indicate that the buffer is empty:
 Token_stream::Token_stream()
-    :full(false), buffer(0)    // no Token in buffer
-{
-}
+    :full(false), buffer(0){ }    // no Token in buffer
+
 
 //------------------------------------------------------------------------------
 
@@ -63,7 +62,7 @@ void Token_stream::putback(Token t)
 
 //------------------------------------------------------------------------------
 
-Token get()
+Token Token_stream:: get()
 {
     if (full) {       // do we already have a Token ready?
         // remove token from buffer
@@ -77,19 +76,25 @@ Token get()
     switch (ch) {
     case ';':    // for "print"
     case 'q':    // for "quit"
-    case '(': case ')': case '+': case '-': case '*': case '/':
+    case '(':
+    case ')':
+    case '+':
+    case '-':
+    case '*':
+    case '/':
         return Token(ch);        // let each character represent itself
     case '.':
     case '0': case '1': case '2': case '3': case '4':
-    case '5': case '6': case '7': case '9':
+    case '5': case '6': case '7': case '8': case '9':
     {
         cin.putback(ch);         // put digit back into the input stream
-        double val;
+        double val = 0;//defind
         cin >> val;              // read a floating-point number
         return Token('8', val);   // let '8' represent "a number"
     }
     default:
         error("Bad token");
+            return 0;
     }
 }
 
@@ -108,17 +113,19 @@ double primary()
 {
     Token t = ts.get();
     switch (t.kind) {
-    case '(':    // handle '(' expression ')'
+    
+        case '(':    // handle '(' expression ')'
     {
         double d = expression();
         t = ts.get();
-        if (t.kind != ')') error("')' expected);
+        if (t.kind != ')') error("')' expected");
             return d;
     }
     case '8':            // we use '8' to represent a number
         return t.value;  // return the number's value
     default:
         error("primary expected");
+            return  0;
     }
 }
 
@@ -135,6 +142,7 @@ double term()
         case '*':
             left *= primary();
             t = ts.get();
+                break;
         case '/':
         {
             double d = primary();
@@ -155,7 +163,7 @@ double term()
 // deal with + and -
 double expression()
 {
-    double left = term(;      // read and evaluate a Term
+    double left = term();      // read and evaluate a Term
     Token t = ts.get();        // get the next token from token stream
 
     while (true) {
@@ -165,7 +173,7 @@ double expression()
             t = ts.get();
             break;
         case '-':
-            left += term();    // evaluate Term and subtract
+            left -= term();    // evaluate Term and subtract
             t = ts.get();
             break;
         default:
@@ -180,7 +188,9 @@ double expression()
 int main()
 try
 {
+    double val = 0;
     while (cin) {
+        
         Token t = ts.get();
 
         if (t.kind == 'q') break; // 'q' for quit
@@ -191,6 +201,7 @@ try
         val = expression();
     }
     keep_window_open();
+    return 0;
 }
 catch (exception& e) {
     cerr << "error: " << e.what() << '\n';
